e515e2c3e3f532fcd5135bfa21946654252d8ae0
Pierre-Henri Merrer <phmerrer@gmail.com>
2023-06-08 12:09:58 +0200

    delete comment

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,10 @@
 import '../style/Banner.css';
-//import Image from "../assets/banner-home.jpg";
-
 
 function Banner({home}) {
     const bannerImageClass = "banner banner" + (home ? "--home" : "--about");
     const bannerTextClass = "banner__h1" + (home ? "--home" : "--about");
     return (
         <div className={bannerImageClass}>
-            {/*<img src={Image} alt='Blabla' className="banner__image" />*/}
             <h1 className={bannerTextClass}>
                 <span>Chez vous, </span>
                 <span>partout et ailleurs</span>
